68c6b6a4903ae184e718414f9d0c7f54d66f3fc9
Nidhi <nidhinayak31@gmail.com>
2024-12-01 18:55:34 +0530

    second commit

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext,useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
-export default function Appointment() {
-  return (
+const Appointment =()=>{
+
+  const {docId} =useParams()
+  const {doctors} = useContext(AppContext)
+  const [doctInfo,setDoctInfo] = useState(null)
+ 
+  useEffect(() => { const fetchDocInfo = async () => { 
+    if (doctors) 
+      { 
+        const docInfo = doctors.find(doc => doc._id === docId); 
+        console.log('Fetched docInfo:', docInfo); setDoctInfo(docInfo);
+      } 
+      else 
+      { 
+        console.error('Doctors data is not available.'); 
+      } 
+    }; 
+    fetchDocInfo(); 
+  }, [doctors, docId]);
+
+
+  return(
     <div>
-      
+
     </div>
   )
+
 }
+
+export default Appointment
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
+
+  const {speciality}=useParams()
+  
+  const[filterDoc,setFilterDoc] = useState([])
+  const navigate = useNavigate()
+  const{doctors} = useContext(AppContext)
+
+  const applyFilter=()=>{
+    if(speciality){
+      setFilterDoc(doctors.filter(doc=>doc.speciality===speciality))
+    }else{
+      setFilterDoc(doctors)
+    }
+  }
+
+  useEffect(()=>{
+    applyFilter()
+  },[doctors,speciality])
   return (
-    <div>
-      
-    </div>
+    <div className='w-full grid grid-cols-auto gap-4 gay-y-6'>
+        {
+          filterDoc.map((item,index)=>(
+            <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duratio-500' key={index}>
+                <img className='bg-blue-50'src={item.image} alt="" />
+                <div className='p-4'>
+                    <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p>
+                        <p>Avaiable</p>
+                    </div>
+                        <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                        <p className='tetx-gray-600 tetx-sm'>{item.speciality}</p>
+                </div>
+                
+            </div>
+        ))
+        }
+      </div>
   )
 }
 
